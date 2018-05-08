e61d6cd083eb145843e8b504d7dbb1bdcee81e4d
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-05-08 06:30:39 -0600

    Defined various events.

---
+++ events/events.go
@@ -0,0 +1,36 @@
+package events
+
+import (
+	"time"
+	"net/url"
+)
+
+type TagAdded struct {
+	TagID     uint64    `json:"tag_id"`
+	Timestamp time.Time `json:"timestamp"`
+	TagName   string    `json:"tag_name"`
+}
+type ManagedAssetImported struct {
+	AssetID   uint64 `json:"asset_id"`
+	Timestamp uint64 `json:"timestamp"`
+	SHA256    []byte `json:"sha256"`
+	MIMEType  string `json:"mime_type"`
+	Name      string `json:"name"`
+}
+type CloudAssetImported struct {
+	AssetID   uint64  `json:"asset_id"`
+	Timestamp uint64  `json:"timestamp"`
+	Name      string  `json:"name,omitempty"`
+	URL       url.URL `json:"url"`
+}
+type DocumentDefined struct {
+	DocumentID  uint64     `json:"document_id"`
+	Timestamp   uint64     `json:"timestamp"`
+	AssetID     uint64     `json:"asset_id"`
+	Published   *time.Time `json:"published,omitempty"`
+	PeriodBegin *time.Time `json:"begin,omitempty"`
+	PeriodEnd   *time.Time `json:"end,omitempty"`
+	Tags        []uint64   `json:"tags,omitempty"`
+	Associated  []uint64   `json:"Associated,omitempty"`
+	Description string     `json:"description,omitempty"`
+}
