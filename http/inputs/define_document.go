8d6ed204e97cdd3ca02372b4b2cc2fcb231afba8
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-05-08 10:16:04 -0600

    Ensuring unique values.

--- http/inputs/define_document.go
+++ http/inputs/define_document.go
@@ -33,12 +33,29 @@ func (this *DefineDocument) Validate() error {
 	errors = errors.AppendIf(beforeMustComeFirstError,
 		this.PeriodBegin != nil && this.PeriodEnd != nil && this.PeriodBegin.After(*this.PeriodEnd))
 	errors = errors.AppendIf(endMustHaveABeforeError, this.PeriodEnd != nil && this.PeriodBegin == nil)
+	errors = errors.AppendIf(duplicateTagError, containsDuplicate(this.Tags))
+	errors = errors.AppendIf(duplicateDocumentError, containsDuplicate(this.Documents))
 	return errors
+}
+
+func containsDuplicate(values []uint64) bool {
+	unique := make(map[uint64]struct{}, len(values))
+	for _, value := range values {
+		if _, contains := unique[value]; contains {
+			return true
+		} else {
+			unique[value] = struct{}{}
+		}
+	}
+
+	return false
 }
 
 var (
 	beforeMustComeFirstError   = fieldError("The begin date must come before the end date.", jsonPeriodBeginField, jsonPeriodEndField)
 	endMustHaveABeforeError    = fieldError("When an end date is specified it must have a begin date.", jsonPeriodBeginField, jsonPeriodEndField)
+	duplicateTagError          = fieldError("The tag IDs provided must be unique.", jsonTagsField)
+	duplicateDocumentError     = fieldError("The document IDs provided must be unique.", jsonDocumentsField)
 	AssetDoesNotExistResult    = notFoundResult("The asset ID supplied could not be found.", jsonAssetIDField)
 	TagDoesNotExistResult      = notFoundResult("One or more tag supplied could not be found.", jsonTagsField)
 	DocumentDoesNotExistResult = notFoundResult("One or more documents supplied could not be found.", jsonDocumentsField)
