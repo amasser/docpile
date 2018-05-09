d6c0708be3304e116375c9680981f04b538cf221
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-05-08 22:15:39 -0600

    Created quick/dirty wireup to see application running.

--- main.go
+++ main.go
@@ -1,5 +1,55 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"bitbucket.org/jonathanoliver/docpile/domain"
+	"bitbucket.org/jonathanoliver/docpile/http"
+	"bitbucket.org/jonathanoliver/docpile/storage"
+	"github.com/julienschmidt/httprouter"
+	"github.com/smartystreets/detour"
+
+	stdhttp "net/http"
+)
+
+const workspacePath = "/Users/jonathan/Downloads/docpile"
+
+func main() {
+	identity := domain.NewEpochGenerator()
+	aggregate := domain.NewAggregate(identity)
+
+	applicator := &Applicator{} // TODO
+
+	var handler domain.Handler = domain.NewMessageHandler(aggregate, applicator)
+	handler = storage.NewLocalStorageHandler(handler, storage.NewLocalStorage(workspacePath))
+
+	tagController := http.NewTagController(handler)
+	assetController := http.NewAssetController(handler)
+	documentController := http.NewDocumentController(handler)
+
+	router := buildRouter()
+	router.Handler("PUT", "/tags", detour.New(tagController.Add))
+	router.Handler("PUT", "/assets", detour.New(assetController.ImportManaged))
+	router.Handler("PUT", "/documents", detour.New(documentController.Define))
+
+	fmt.Println("Listening...")
+	stdhttp.ListenAndServe("127.0.0.1:8080", router)
+}
+
+func buildRouter() *httprouter.Router {
+	router := httprouter.New()
+	router.HandleMethodNotAllowed = true
+	router.HandleOPTIONS = true
+	router.RedirectTrailingSlash = false
+	router.RedirectFixedPath = false
+	return router
+}
+
+type Applicator struct{}
+
+func (this *Applicator) Apply(messages ...interface{}) {
+	for _, message := range messages {
+		log.Println("Applying:", message)
+	}
 }
