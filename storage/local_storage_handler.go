9975db3f7dd09c8aceb1fc61c6698d9e4394f594
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-05-08 21:59:10 -0600

    Spooling file to memory and then saving to disk.

--- storage/local_storage_handler.go
+++ storage/local_storage_handler.go
@@ -1,6 +1,15 @@
 package storage
 
-import "bitbucket.org/jonathanoliver/docpile/domain"
+import (
+	"bytes"
+	"crypto/sha256"
+	"io"
+	"io/ioutil"
+	"strconv"
+
+	"bitbucket.org/jonathanoliver/docpile/domain"
+	"bitbucket.org/jonathanoliver/docpile/events"
+)
 
 type LocalStorageHandler struct {
 	inner     domain.Handler
@@ -20,15 +29,48 @@ func (this *LocalStorageHandler) Handle(message interface{}) (uint64, error) {
 		return this.inner.Handle(message)
 	}
 }
+
 func (this *LocalStorageHandler) handleImportManagedStreamingAsset(message domain.ImportManagedStreamingAsset) (uint64, error) {
-	return 0, nil
-	//// get temp filename
-	//// write to that location and compute hash while doing so
-	//// pipe through domain using new command
-	//// move file to permanent location
-	//// if fail, send DeleteManagedAsset
-	//return this.inner.Handle(domain.ImportManagedAsset{
-	//	Name:     message.Name,
-	//	MIMEType: message.MIMEType,
-	//})
+	buffer, err := bufferStream(message.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := this.sendMessage(message.Name, message.MIMEType, buffer)
+	if err != nil {
+		return 0, err
+	}
+
+	if err := this.writeBuffer(id, buffer); err != nil {
+		panic(err) // because we don't have a compensating event, we panic so the event never happens
+	}
+
+	return id, nil
+}
+
+func bufferStream(reader io.ReadCloser) (*bytes.Buffer, error) {
+	defer reader.Close()
+	buffer := bytes.NewBuffer([]byte{})
+	if _, err := io.Copy(buffer, reader); err == nil {
+		return buffer, nil
+	} else {
+		return nil, err
+	}
+}
+
+func (this *LocalStorageHandler) sendMessage(name, mime string, buffer *bytes.Buffer) (uint64, error) {
+	return this.Handle(domain.ImportManagedAsset{
+		Name:     name,
+		MIMEType: mime,
+		Hash:     computeHash(buffer),
+	})
+}
+
+func computeHash(buffer *bytes.Buffer) events.SHA256Hash {
+	return events.SHA256Hash(sha256.Sum256(buffer.Bytes()))
+}
+func (this *LocalStorageHandler) writeBuffer(id uint64, buffer *bytes.Buffer) error {
+	filename := strconv.FormatUint(id, 10)
+	source := ioutil.NopCloser(buffer)
+	return this.writer.Write(filename, source)
 }
