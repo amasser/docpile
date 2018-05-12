edf336980da9e48e353f27ed614345abfe9cffe7
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-05-12 13:18:59 -0600

    Search criteria.

--- app/projections/document_search.go
+++ app/projections/document_search.go
@@ -27,5 +27,42 @@ func NewDocumentSearch(
 }
 
 func (this *DocumentSearch) IsSatisfiedBy(document Document) bool {
+	// TODO: period min/max
+	return this.withinPublishedLimits(document.Published) &&
+		documentContainsAllSearchTags(this.tags, document.Tags)
+}
+
+func (this *DocumentSearch) withinPublishedLimits(published *time.Time) bool {
+	if published == nil {
+		return true
+	}
+
+	if this.publishedMin != nil && this.publishedMin.After(*published) {
+		return false
+	}
+
+	if this.publishedMax != nil && this.publishedMax.Before(*published) {
+		return false
+	}
+
 	return true
+}
+
+func documentContainsAllSearchTags(searchTags, documentTags []uint64) bool {
+	for _, searchTag := range searchTags {
+		if !documentTagsContainSearchTag(searchTag, documentTags) {
+			return false
+		}
+	}
+
+	return true
+}
+func documentTagsContainSearchTag(searchTag uint64, documentTags []uint64) bool {
+	for _, documentTag := range documentTags {
+		if documentTag == searchTag {
+			return true
+		}
+	}
+
+	return false
 }
