d2ee10bbe7b87ed2ed985021852fd0c35e577221
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-05-28 18:59:15 -0600

    Extracted state.

--- app/projections/tag_search.go
+++ app/projections/tag_search.go
@@ -7,12 +7,15 @@ type TagSearch struct {
 	tagIndexByID map[uint64]int
 	allTags      []Tag
 
-	searchText         string
-	selectedTagIDs     []uint64
+	searchText     string
+	selectedTagIDs []uint64
+
+	candidates         map[uint64]struct{}
 	candidateTagIDs    []uint64
 	candidateTagValues []string
-	fuzzyMatches       []fuzzy.Match
-	results            []MatchingTag
+
+	fuzzyMatches []fuzzy.Match
+	results      []MatchingTag
 }
 
 func NewTagSearch(documents []Document, tagIndexByID map[uint64]int, allTags []Tag) *TagSearch {
@@ -20,6 +23,7 @@ func NewTagSearch(documents []Document, tagIndexByID map[uint64]int, allTags []T
 		allDocs:      documents,
 		tagIndexByID: tagIndexByID,
 		allTags:      allTags,
+		candidates:   make(map[uint64]struct{}),
 	}
 }
 
@@ -28,36 +32,34 @@ func (this *TagSearch) Search(searchText string, selectedTagIDs []uint64) []Matc
 	this.selectedTagIDs = selectedTagIDs
 
 	this.gatherCandidates()
+	this.removeSelectedTags()
+	this.addCandidates()
+
 	this.conductSearch()
 	this.renderResults()
 	return this.results
 }
 
 func (this *TagSearch) gatherCandidates() {
-	candidates := make(map[uint64]struct{})
 	criteria := NewDocumentCriteria(nil, nil, nil, nil, this.selectedTagIDs)
 	for _, doc := range this.allDocs {
-		if !criteria.Match(doc) {
-			continue
-		}
-
-		for _, tagID := range doc.Tags {
-			candidates[tagID] = struct{}{}
+		if criteria.Match(doc) {
+			for _, tagID := range doc.Tags {
+				this.candidates[tagID] = struct{}{}
+			}
 		}
 	}
-
-	this.removeSelectedTags(candidates)
-	this.addCandidates(candidates)
 }
-func (this *TagSearch) removeSelectedTags(candidates map[uint64]struct{}) {
+func (this *TagSearch) removeSelectedTags() {
 	for _, selectedTagID := range this.selectedTagIDs {
-		delete(candidates, selectedTagID)
+		delete(this.candidates, selectedTagID)
 	}
 }
-func (this *TagSearch) addCandidates(candidates map[uint64]struct{}) {
-	this.candidateTagIDs = make([]uint64, 0, len(candidates))
-	this.candidateTagValues = make([]string, 0, len(candidates))
-	for tagID := range candidates {
+func (this *TagSearch) addCandidates() {
+	this.candidateTagIDs = make([]uint64, 0, len(this.candidates))
+	this.candidateTagValues = make([]string, 0, len(this.candidates))
+
+	for tagID := range this.candidates {
 		this.addCandidate(tagID)
 	}
 }
